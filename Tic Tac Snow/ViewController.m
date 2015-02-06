c1b647e678e46b76ea0ad1a962e35e21b052b076
ajchang <ajchang@ditto.cs.uchicago.edu>
2015-02-05 22:58:27 -0600

    added intersection check

--- Tic Tac Snow/ViewController.m
+++ Tic Tac Snow/ViewController.m
@@ -10,7 +10,7 @@
 
 @interface ViewController ()
 
-
+@property NSInteger currentPlayer;
 
 @end
 
@@ -18,22 +18,14 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
+
     // Do any additional setup after loading the view, typically from a nib.
     
-    self.gridView.contentMode = UIViewContentModeScaleAspectFit;
-    [self.gridView setImage:[UIImage imageNamed:@"grid"]];
-    
     self.imageArray = [[NSMutableArray alloc] initWithCapacity:9];
-    
-    //build array of imageviews for the grid spaces
-    int i;
-    for (i=0; i < 9; i++) {
-        UIView *space = [[UIView alloc] init];
-        [self.imageArray addObject:space];
+
+    for (int i=0; i<9; i++) {
+        [self.imageArray addObject:[self.view viewWithTag:i]];
     }
-    
-    [self.x setImage:[UIImage imageNamed:@"x"]];
-    [self.o setImage:[UIImage imageNamed:@"o"]];
     
     //note: MUST do this programmatically!!! for some reason doesn't work when i add the pangesture in storyboard...
     UIPanGestureRecognizer *panGesture = [[UIPanGestureRecognizer alloc] initWithTarget:self action:@selector(handlePan:)];
@@ -42,6 +34,11 @@
     [self.x addGestureRecognizer:panGesture];
     [self.o addGestureRecognizer:panGesture];
     panGesture = nil;
+    
+    //X gets the first move.
+    _currentPlayer = 1;
+    
+    [self makeMove];
 }
 
 - (void)didReceiveMemoryWarning {
@@ -59,6 +56,68 @@
                                          sender.view.center.y + translation.y);
     [sender setTranslation:CGPointMake(0, 0) inView:self.view];
   }
+
+- (XO*)getCurrentPlayer {
+    
+    
+    if (_currentPlayer == 1) {
+        return self.x;
+    }
+    else {
+        return self.o;
+    }
+}
+
+//Source: http://stackoverflow.com/questions/22395712/making-an-animation-to-expand-and-shrink-an-uiview
+- (void)currentPlayerAnimation {
+    XO* player = [self getCurrentPlayer];
+    
+    [UIView animateWithDuration:1.5
+                     animations:^{
+                         player.transform = CGAffineTransformMakeScale(1.5, 1.5);
+                     }
+                     completion:^(BOOL finished) {
+                         [UIView animateWithDuration:1
+                                          animations:^{
+                                              player.transform = CGAffineTransformIdentity;
+                                              
+                                          }];
+                     }];
+}
+
+- (void)makeMove {
+    [self currentPlayerAnimation];
+    XO* player = [self getCurrentPlayer];
+    //after pan ended...
+    for (int i=0; i<9; i++) {
+        //where the player's piece was dropped
+        CGRect playerFrame = [player frame];
+        //the space on the grid you are currently checking
+        CGRect imageFrame = [self.imageArray[i] frame];
+        
+        //intersection between the piece's drop position and current grid space
+        CGRect intersection = CGRectIntersection(playerFrame, imageFrame);
+        
+        if (CGRectIsNull(intersection)) {
+            //not touching
+        }
+        else {
+            //touching!
+        }
+    }
+}
+
+
+
+- (void)switchPlayer {
+    
+    if (_currentPlayer == 1) {
+        _currentPlayer = 0;
+    }
+    else {
+        _currentPlayer = 1;
+    }
+}
 
 - (bool)gameOver {
     //if 3 in a row, then game over
